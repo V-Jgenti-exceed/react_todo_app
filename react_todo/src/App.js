6edecec76a53b8d481c80a0d5251e746e325e3ab
V-Jgenti-exceed <v.jgenti.exeedteam@gmail.com>
2020-04-23 11:43:56 +0400

    React_todo

---
+++ react_todo/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+
+
+
+const App = () => {
+  return (
+    <React.Fragment>
+     
+    </React.Fragment>
+  )
+}
+
+
+
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+export default App;
