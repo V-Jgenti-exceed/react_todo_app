ce1716e5c99149c327fc0fa5d0a65ebb9a5718c3
V-Jgenti-exceed <v.jgenti.exeedteam@gmail.com>
2020-05-25 11:08:54 +0400

    modified server.js file

--- react_todo/server.js
+++ react_todo/server.js
@@ -13,6 +13,9 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.get('/', function (req, res) {
+    res.redirect('/todo');
+});
 app.listen(port, () => {
-console.log('server is up and working on port' + port);
+    console.log('server is up and working on port' + port);
 });
