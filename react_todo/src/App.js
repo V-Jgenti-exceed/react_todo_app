4eceb426b46787363fafce568ab5adb2e2c1ebb3
V-Jgenti-exceed <v.jgenti.exeedteam@gmail.com>
2020-05-25 12:54:01 +0400

    modified app.js for correct working with bootstrap

--- react_todo/src/App.js
+++ react_todo/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Itemslist } from '../src/containers/Itemslist';
 import { AddItem } from '../src/components/AddItem';
 
